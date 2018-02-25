375d64fff951356ec79a944a84708ef7f445b028
Charlotte Payne <cltpayne@gmail.com>
2018-02-25 14:41:18 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,20 +1,5 @@
 // declare your functions here...
+
 function paragraphSelector() {
-  return $('p');
-}
-
-function lastImageSelector() {
-  $('image:last');
-}
-
-function ninjaBabySelector() {
-  $("#baby-ninja");
-}
-
-function divSelector() {
-  $("div .pics");
-}
-
-function firstListItem() {
-  $("ul:first-child #pic-list");
+  return $('p')
 }
